0204017145b15a0288806c85c39c65ec2f3a9d14
Valentin Conchis <151737381+valentinConchis@users.noreply.github.com>
2024-06-14 11:09:51 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 contact: resolve("./contact.html"),
                 formation: resolve("./formation.html"),
                 competences: resolve("./competences.html"),
+                competence1: resolve("./competence1.html"),
                 competence2: resolve("./competence2.html"),
                 competence3: resolve("./competence3.html"),
                 competence4: resolve("./competence4.html"),
